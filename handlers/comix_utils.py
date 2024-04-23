d17afcbc63cd54dec40a419c587467aa21d175a0
14Fleksss88 <131782632+Fleksozavr@users.noreply.github.com>
2024-04-23 17:18:38 +0500

    Update comix_utils.py
    Убрал class, исправил логику, теперь функции принимают в качестве аргумента рандомное число

--- handlers/comix_utils.py
+++ handlers/comix_utils.py
@@ -1,43 +1,33 @@
 import requests
 import random
-from random import randint
-from aiogram import Router, F, types
+from aiogram import Router, types
 
 
-class Image:
+def get_random_num():
+    response = requests.get('https://xkcd.com/info.0.json')
+    response.raise_for_status()
+    max_num = response.json()['num']
+    return random.randint(1, max_num)
 
 
-    def get_random_num(self):
-        response = requests.get('https://xkcd.com/info.0.json')
-        response.raise_for_status()
-        max_num = response.json()['num']
-        return random.randint(1, max_num)
+def download_comix(random_num):
+    url = f'https://xkcd.com/{random_num}/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    try:
+        data = response.json()
+        image_url = data['img']
+        return image_url
+    except AttributeError as err:
+        print('Ошибка при извелчении атрибута:', err)
 
 
-    def download_comix(self):
-        random_num = self.get_random_num()
-        url = f'https://xkcd.com/{random_num}/info.0.json'
-        response = requests.get(url)
-        response.raise_for_status()
-
-        try:
-            data = response.json()
-            image_url = data['img']
-            return image_url
-
-        except Exception as err:
-            print('Error when receiving picture:', err)
-
-
-    def get_author_comment(self):
-        random_num = self.get_random_num()
-        url = f'https://xkcd.com/{random_num}/info.0.json'
-        response = requests.get(url)
-        response.raise_for_status()
-
-        try:
-            author_text = response.json()['alt']
-            return author_text
-
-        except Exception as err:
-            print('Error when receiving text:', err)
+def get_author_comment(random_num):
+    url = f'https://xkcd.com/{random_num}/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    try:
+        author_text = response.json()['alt']
+        return author_text
+    except AttributeError as err:
+        print('Ошибка при извелчении атрибута:', err)
