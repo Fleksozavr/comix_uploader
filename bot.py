bc4df73ee7f5c5f2ed26630ec36d082b814972ff
14Fleksss88 <131782632+Fleksozavr@users.noreply.github.com>
2024-04-23 17:15:37 +0500

    Update bot.py
    была переделана логика

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 import os
-from dotenv import load_dotenv
 import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import start, comix_utils, comix
-
-
-load_dotenv()
+from dotenv import load_dotenv
 
 
 async def main():
-    bot = Bot(token=os.getenv("BOT_TOKEN"))
+    load_dotenv()
+    bot_token = os.getenv("TG_BOT_TOKEN")
+
+    if not bot_token or not chat_id:
+        raise ValueError("Необходимо заполнить переменные окружения TG_BOT_TOKEN и CHAT_ID")
+
+    bot = Bot(token=bot_token)
     dp = Dispatcher()
-
     dp.include_routers(start.router, comix.router)
-
+    
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
